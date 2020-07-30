aee729412cf07b0899da1bcad185a6517e4b4f8e
dako0819 <daniil.konstantinov@netcracker.com>
2020-07-30 22:36:23 +0300

    add form for saving images

---
+++ images/forms.py
@@ -0,0 +1,51 @@
+from urllib import request
+from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+from .models import Image
+
+
+class ImageCreateForm(forms.ModelForm):
+    # используем ModelForm модели Image, которая включает поля title, url, description. Пользователи не будут вручную
+    # заполнять адрес. Вместо этого мы добавим JavaScript-инструмент для выбора картинки на любом постороннем сайте,
+    # а наша форма будет получать URL изображения в качестве параметра.
+    # Мы заменили виджет по умолчанию для поля url и используем HiddenInput. Этот виджет формируется как input-элемент
+    # с атрибутом type="hidden". Мы сделали это для того, чтобы пользователи не видели поле <url>.
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {'url': forms.HiddenInput, }
+
+    # метод clean_url() для валидации поля url. Django дает возможность проверять каждое поле формы по отдельности
+    # с помощью методов вида clean_<fieldname>()
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
+
+    # Этот метод принимает необязательный аргумент <commit>, который позволяет настроить поведение (действительно ли
+    # нужно сохранять объект в базу данных). Если commit равен False, метод вернет объект модели, но не сохранит его.
+    # Мы переопределим save() формы, чтобы скачивать файл картинки и сохранять его.
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)  # creates image but not saves it
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title),
+                                    image_url.rsplit('.', 1)[1].lower())
+        # Скачиваем изображение по указанному адресу.
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+        if commit:
+            image.save()
+        return image
+# Данная функция
+# - создает объект image, вызвав метод save() с аргументом commit=False;
+# - получает URL из атрибута cleaned_data формы;
+# - генерирует название изображения, совмещая слаг и расширение картинки;
+# - использует Python-пакет urllib, чтобы скачать файл картинки, и вызывает метод save() поля изображения, передавая
+# в него объект скачанного файла, ContentFile. Используется commit=False, чтобы покане сохранять объект в базу данных;
+# - при переопределении метода важно оставить стандартное поведение, поэтому сохраняем объект изображения в базу данных
+# только в том случае, если commit равен True.
+
--- images/views.py
+++ images/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # Форма отправлена.
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            # Добавляем пользователя к созданному объекту.
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            # Перенаправляем пользователя на страницу сохраненного изображения.
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request, 'images/image/create.html',
+                  {'section': 'images', 'form': form})
+# Этот обработчик выполняет следующие действия:
+# - получает начальные данные и создает объект формы. Эти данные содержат url и title картинки со стороннего сайта, они
+# будут переданы в качестве аргументов GET-запроса JavaScript-инструментом, который мы добавим чуть позже.
+# - если форма отправлена POST-запросом, проверяет ее корректность. Если данные валидны, создает новый объект Image,
+# но пока не сохраняет его в базу данных, передавая аргумент commit=False;
+# - привязывает текущего пользователя к картинке. Так мы узнаем, кто загрузил это изображение;
+# - сохраняет объект image в базу данных;
+# - наконец, создает уведомление и перенаправляет пользователя на канонический URL новой картинки. Мы пока
+# не реализовали метод get_absolute_url() для модели Image, но добавим его чуть позже.
